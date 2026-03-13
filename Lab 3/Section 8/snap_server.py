6971aa4cf88c07296abb6c9eb201eb901b451670
arnaramirez <arnaramirez@berkeley.edu>
2026-03-13 14:50:09 -0700

    Update snap_server.py

--- Lab 3/Section 8/snap_server.py
+++ Lab 3/Section 8/snap_server.py
@@ -2,8 +2,10 @@
 from snap_spec.snap import UGRadioSnap
 
 # Initialize the SNAP board server
-snap = UGRadioSnap(host='0.0.0.0', is_discover=True)
+snap = UGRadioSnap(host='localhost', is_discover=True)
 snap.initialize(mode='corr', sample_rate=500)
 
 print("SNAP server running. Waiting for client connections...")
 snap.run()  # keeps the server alive to accept connections from clients like snap_test.py
+
+
